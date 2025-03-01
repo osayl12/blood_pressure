295ca1b4f85f6e4094bf2ac10bee05f96401a62a
osayl12 <hamedosayl@gmail.com>
2025-03-01 18:59:03 +0200

    הוספת מסמך swagger לקובץ Summary

--- Routers/Summary_R.js
+++ Routers/Summary_R.js
@@ -2,7 +2,47 @@ const express = require('express');
 const router = express.Router();
 const summaryMid = require("../Middleware/Summary_Mid");
 
-
+/**
+ * @swagger
+ * /summary/monthly:
+ *   get:
+ *     summary: Get monthly summary for all users
+ *     tags: [Summary]
+ *     parameters:
+ *       - in: query
+ *         name: month
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Month in format YYYY-MM
+ *     responses:
+ *       200:
+ *         description: Monthly summary retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       user:
+ *                         type: string
+ *                       avgSystolic:
+ *                         type: number
+ *                       avgDiastolic:
+ *                         type: number
+ *                       avgPulse:
+ *                         type: number
+ *                       abnormalCount:
+ *                         type: number
+ *       500:
+ *         description: Error retrieving summary
+ */
 router.get("/monthly", [summaryMid.GetMonthlySummary], (req, res) => {
     if (req.success) {
         res.status(200).json({ msg: "ok", data: req.summary });
